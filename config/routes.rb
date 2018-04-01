9dea33fc7228f9b391acdb8ce6005b2fc6139ce2
anand2905 <anand29.ku@gmail.com>
2018-04-01 16:43:13 +0530

    Moved the contact form with partials and added contact update method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 	get "contacts/index", as: 'contacts'
 	get 'contacts/new', as: 'new_contacts'
-	post '/contacts/create' 
-	root 'contacts#index'
+	post 'contacts/create', as: 'create_contact'
+  get 'contacts/:id/edit', to: 'contacts#edit', as: 'edit_contact'
+	patch 'contacts/:id/update', to: 'contacts#update', as: 'update_contact'
+
+  root 'contacts#index'
 end
