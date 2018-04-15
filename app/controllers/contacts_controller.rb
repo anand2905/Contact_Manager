c456ff0a3cc532a9253277395426e90dd72d8f4d
anand2905 <anand29.ku@gmail.com>
2018-04-15 23:09:23 +0530

    Added search form feature

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,18 @@
 class ContactsController < ApplicationController
+  before_action :find_contact, only: [:edit, :update, :destroy]
 
 	def index
+		session[:selected_group_id] = params[:group_id]
 		if params[:group_id] && !params[:group_id].empty?
-				@contacts = Contact.where(group_id: params[:group_id]).contacts.order(created_at: :desc).page(params[:page])
+				#@contacts = Contact.where(group_id: params[:group_id]).contacts.order(created_at: :desc).page(params[:page])
+        group = Group.find(params[:group_id])
+        if params[:terms] && !params[:terms].empty?
+          @contacts = group.contacts.where('name LIKE ?', "%#{params[:term]}%").order(created_at: :desc).page(params[:page])
+        else
+				  @contacts = group.contacts.order(created_at: :desc).page(params[:page])
+      end
 		else
-			@contacts = Contact.order(created_at: :desc).page(params[:page])
+			@contacts = Contact.where('name LIKE ?', "%#{params[:term]}%").order(created_at: :desc).page(params[:page])
 		end
 	end
 
@@ -16,28 +24,25 @@ class ContactsController < ApplicationController
 		@contact = Contact.new(contact_params)
 			if @contact.save
 				flash[:success] = "Contact was successfully created."
-				redirect_to contacts_path
+				redirect_to contacts_path(previous_query_string)
 			else
 				render 'new'
 		end
 	end
 
 	def edit
-		@contact = Contact.find(params[:id])
 	end
 
 	def update
-		@contact = Contact.find(params[:id])
 		if @contact.update(contact_params)
 			flash[:success] = "Contact was successfully updated"
-			redirect_to contacts_path
+			redirect_to contacts_path(previous_query_string)
 		else
 			render 'edit'
 		end
 	end
 
 	def destroy
-		@contact = Contact.find(params[:id])
 		@contact.destroy
 		flash[:success] = "Contact was successfully deleted"
 		redirect_to contacts_path
@@ -48,4 +53,12 @@ class ContactsController < ApplicationController
 	def contact_params
 		params.require(:contact).permit(:name, :email, :company, :address, :phone, :group_id, :avatar)
 	end
+
+	def previous_query_string
+		session[:selected_group_id] ? { group_id: session[:selected_group_id]} : {}
+	end
+
+  def find_contact
+    @contact = Contact.find(params[:id])
+  end
 end
