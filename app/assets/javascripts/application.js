634e1ce8f9b9f33f08b9ef15ce0bf093719b1d83
anand2905 <anand29.ku@gmail.com>
2018-04-21 12:45:09 +0530

    Implemented jason response using jbuilder

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,11 @@
 //= require bootstrap-sprockets
 //= require jasny-bootstrap.min
 //= require turbolinks
-//= require jquery-ui
+//= require jquery-ui/core
+//= require jquery-ui/widget
+//= require jquery-ui/position
+//= require jquery-ui/widgets/autocomplete
+//= require jquery-ui/widgets/menu
 //= require_tree .
 
 $(function(){
@@ -24,6 +28,7 @@ $(function(){
 			minLength: 3,
 			select: function (event, ui) {
 				$('#term').val(ui.item.value);
+				$(this).closest('form').submit();
 			}
 		});
 	});
