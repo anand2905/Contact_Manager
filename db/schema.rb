4c6d966944595cdd22e42637083e77e471054c17
anand2905 <anand29.ku@gmail.com>
2018-04-28 12:25:05 +0530

    Added User references to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180421173109) do
+ActiveRecord::Schema.define(version: 20180428064804) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
@@ -25,11 +25,15 @@ ActiveRecord::Schema.define(version: 20180421173109) do
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.integer "user_id"
     t.index ["group_id"], name: "index_contacts_on_group_id"
+    t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
